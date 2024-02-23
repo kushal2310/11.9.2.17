9a15a12035118fd9dcdfa0f7ab88acc54621e388
kushal2310 <155981353+kushal2310@users.noreply.github.com>
2024-02-23 16:49:41 +0530

    Add files via upload

--- 17/codes/plotting.py
+++ 17/codes/plotting.py
@@ -8,7 +8,7 @@ y_values = data[:,1]
 #print(x_values) Used while debugging
 plt.stem(x_values,y_values)
 plt.scatter(29, 245, color = 'green')
-plt.xlabel('t')
-plt.ylabel('y(t)')
+plt.xlabel('n')
+plt.ylabel('x(n)')
 plt.grid(True)
-plt.savefig('y(t)_vs_t.png')
+plt.savefig('x(n)_vs_n.png')
